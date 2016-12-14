24b0860882d4e6de4766a4656323f4be89cdf412
Cassie <cassie.pericak@northwestern.edu>
2016-12-13 20:20:56 -0600

    linked to packages

---
+++ liri.js
@@ -0,0 +1,7 @@
+//require npm packages
+var request = require('request');
+var twitter = require('twitter');
+var spotify = require('spotify');
+var inquirer = require('inquirer');
+var keys = require('./keys.js');
+var fs = require('fs');
