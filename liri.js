373362a826547e3e3966900ab54dddf8c83f2aac
Cassie <cassie.pericak@northwestern.edu>
2016-12-16 21:20:44 -0600

    created for loops to loop through all of the data

--- liri.js
+++ liri.js
@@ -29,10 +29,14 @@ if (action === "my-tweets") {
 
         //if there isn't an error, log the tweets
         if (!error) {
-            console.log(tweets);
-
+            var allTweets = "";
+            for (var i = 0; i < tweets.length; i++) {
+                var tweetsData = tweets[i].text;
+                allTweets += tweetsData;
+                console.log(tweetsData);
+            }
             //append the text file data
-            fs.appendFile("random.txt", /*DATA,*/ "utf8", function(error) {
+            fs.appendFile("random.txt", allTweets, "utf8", function(error) {
 
                 //if there is an error, log the error
                 if (error) {
@@ -61,10 +65,15 @@ else if (action === "spotify-this-song") {
 
             //otherwise log the necessary information for the song "The Sign"
         } else {
-            var songData = data.tracks.items[0];
-            console.log(songData.artists[0].name + "\n" + songData.name + "\n" + songData.album.name + "\n" + songData.preview_url);
-
-            fs.appendFile("random.txt", /*DATA,*/ "utf8", function(error) {
+            var songs = data.tracks.items;
+            var allSongs = "";
+            for (var i = 0; i < songs.length; i++) {
+                var songData = songs[i];
+                var songInfo = songData.artists[0].name + "\n" + songData.name + "\n" + songData.preview_url + "\n" + songData.album.name;
+                allSongs += songInfo;
+                console.log(songInfo);
+            }
+            fs.appendFile("random.txt", allSongs, "utf8", function(error) {
 
                 //if there is an error, log the error
                 if (error) {
@@ -80,12 +89,20 @@ else if (action === "movie-this") {
     //use request to query the omdb api and search by the name variable or if there is no 3rd argument, search for Mr. Nobody and return the data as a json object
     request("http://www.omdbapi.com/?t=" + name || "Mr. + Nobody" + "&y=&plot=short&r=json", function(error, response, body) {
 
+        var movies = body;
+        var allMovies = "";
+
         //if there isn't any error and the response status code is in the 200s, log the data
         if (!error && response.statusCode == 200) {
-            console.log(JSON.parse(body));
+            for (var i = 0; i < movies.length; i++) {
+                var movieData = movies[i];
+                var movieInfo = JSON.parse(body);
+                allMovies += movieInfo;
+                console.log(movieInfo);
+            }
         }
         //append the text file with the data
-        fs.appendFile("random.txt", /*DATA,*/ "utf8", function(error) {
+        fs.appendFile("random.txt", allMovies, "utf8", function(error) {
 
             //if there is an error, log the error
             if (error) {
@@ -100,9 +117,14 @@ else if (action === "do-what-it-says") {
 
     //use fs to read the text file
     fs.readFile('./random.txt', 'utf8', function(err, data) {
+
+        //if there is an error
         if (err) {
+
+            //exit the function and log the error
             return console.log(err);
         }
+        //log the data
         console.log(data);
     });
 }
